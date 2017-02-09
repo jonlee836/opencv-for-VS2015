1392ce981e436b948856e28ffd06ffcfbcca2c9e
Jon Lee <jonlee836@gmail.com>
2017-02-09 01:11:45 -0800

    no message

--- opencvTest/thresholdImage.cpp
+++ opencvTest/thresholdImage.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 void threshImage::colorspace(Mat& a) {
 
-	//cv::imshow("window", a);
+	cv::imshow("window", a);
 
-	cvtColor(a, a, CV_BGR2GRAY);
+	cvtColor(a, a, CV_BGR2HSV);
 	//imshow("gray", a);
-	/*split(a, LuvChannels);
+	split(a, LuvChannels);
 
 	imshow("L channel", LuvChannels[0]);
 	imshow("U channel", LuvChannels[1]);
-	imshow("V channel", LuvChannels[2]);*/
+	imshow("V channel", LuvChannels[2]);
 
 }
